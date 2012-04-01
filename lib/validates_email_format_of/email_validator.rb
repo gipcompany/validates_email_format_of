91898bde856516e59958f79adc60fde087b2cd41
Jonathan <jonathan@asus.(none)>
2012-04-01 13:24:24 +0200

    Bugfix : allow empty email

--- lib/validates_email_format_of/email_validator.rb
+++ lib/validates_email_format_of/email_validator.rb
@@ -5,7 +5,7 @@ class EmailValidator < ActiveModel::EachValidator
 	cattr_accessor :whitelist_domains
 
 	def validate_each(record, attribute, value)
-		return if value.nil?
+		return if value.blank?
 		value = value.to_s.downcase
 		unless value =~ /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i
 			record.errors[attribute] << (options[:message] || I18n.t('errors.messages.not_an_email_adress'))
